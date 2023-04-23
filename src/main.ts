0592a294f2f762a7dcadd012bcadba5789bdb913
Yuji Nakamura <ny5117@icloud.com>
2023-04-23 22:34:22 +0900

    swaggerを導入

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const options = new DocumentBuilder()
+    .setTitle('Awesomemory API仕様書')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
