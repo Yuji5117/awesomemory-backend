95757becbcf495542ab329bb94a2b3bf96208070
Yuji Nakamura <ny5117@icloud.com>
2023-05-10 22:34:59 +0900

    authGuardを実装し、認証済みでない場合は、401を返すように実装

--- src/presenter/auth/auth.controller.ts
+++ src/presenter/auth/auth.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthUseCases } from '@/use-cases/auth';
 import { SignInDTO } from '@/domain/auth/dtos';
+import { AuthGuard } from '@/infrastructure/libs/guard';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +20,11 @@ export class AuthController {
   @Post('login')
   signIn(@Body() signInDTO: SignInDTO) {
     return this.authUseCases.signIn(signInDTO.username, signInDTO.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
